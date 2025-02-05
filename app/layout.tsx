d69704891ded567353c9f77542cb41fa8f3fde8e
Thien Nguyen <tatthien.contact@gmail.com>
2025-02-05 09:31:27 +0700

    feat: initialize project

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NFT Marketplace",
+  description: "NFT Marketplace",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
