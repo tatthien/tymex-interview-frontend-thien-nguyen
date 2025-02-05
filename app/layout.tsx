ec371740db2de1a47477782f4f7d5f3ea92af3d3
Thien Nguyen <tatthien.contact@gmail.com>
2025-02-05 21:34:48 +0700

    chore: add prettier + eslint plugin simple import sort

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,26 @@
-import type { Metadata } from "next";
-import { Geist } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Geist } from 'next/font/google'
+
+import './globals.css'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "NFT Marketplace",
-  description: "NFT Marketplace",
-};
+  title: 'NFT Marketplace',
+  description: 'NFT Marketplace',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable}`}>
-        {children}
-      </body>
+      <body className={`${geistSans.variable}`}>{children}</body>
     </html>
-  );
+  )
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,59 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { FlatCompat } from '@eslint/eslintrc'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+    },
+  },
+  // Translate the eslintrc format into flat config format
+  ...compat.config({
+    overrides: [
+      {
+        files: ['*.mjs', '*.js', '*.jsx', '*.ts', '*.tsx'],
+        rules: {
+          'simple-import-sort/imports': [
+            'error',
+            {
+              groups: [
+                // Node.js modules with `node:` prefix
+                ['^node:'],
+                ['^react', '^next', '^@?\\w'],
+                // Internal packages
+                ['^(@|components)(/.*|$)'],
+                // Side effect imports
+                ['^\\u0000'],
+                // Parent imports
+                ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+                // Other relative imports
+                ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+                // Style imports
+                ['^.+\\.?(css)$'],
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
