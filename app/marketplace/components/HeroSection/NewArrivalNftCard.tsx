8d410ef481a63ac962a25295e3d8b79830b85328
Thien Nguyen <tatthien.contact@gmail.com>
2025-02-06 16:26:44 +0700

    chore: add animation when hover to new arrival nft cards

--- app/marketplace/components/HeroSection/NewArrivalNftCard.tsx
+++ app/marketplace/components/HeroSection/NewArrivalNftCard.tsx
@@ -11,13 +11,15 @@ export function NewArrivalNftCard({ image, name }: NewArrivalNftCardProps) {
   return (
     <div className={styles.nftCard}>
       <div className={styles.nftCardImageWrapper}>
-        <Image
-          src={image}
-          className={styles.nftCardImage}
-          alt={name}
-          width={200}
-          height={200}
-        />
+        <div style={{ overflow: 'hidden' }}>
+          <Image
+            src={image}
+            className={styles.nftCardImage}
+            alt={name}
+            width={200}
+            height={200}
+          />
+        </div>
       </div>
       <p className={styles.nftCardName}>{name}</p>
     </div>
